70ff16657f088ac56086c3c7e6e775a948326802
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-02-11 12:11:29 +0200

    Update 01. Disneyland Journey.js

--- exam/12.10/01. Disneyland Journey.js
+++ exam/12.10/01. Disneyland Journey.js
@@ -1,27 +1,32 @@
-function solve(a,b){
-   let salary = a
-   let month = b
+function solve(input){
+   let salary = input.shift()
+   let month = input.shift()
+   
+let sum = []
+for (let i = 1; i <= month; i++) {
+    sum.push(i)
 
-   let sum = []
- 
-   for (let i = 1; i <= month; i++) {
-   sum.push(i)
-    if(sum.includes(3)){
-        let three = (salary * 2 * 0.25) * 0.16;
-        let totalThree = (salary * 0.25)  -three;
-        var finalThree = totalThree + (salary * 2 * 0.25)
-    }
-    else if(sum.includes(4)){
-        var four = (finalThree * 0.25) + salary * 0.25
-    }
-    let finish = finalThree+four
-    if(finish>salary){
-        let end = finish - salary;
-        console.log(`Bravo! You can go to Disneyland and you will have ${end.toFixed(2)}lv. for souvenirs.`)
-    }else if(finish<salary){
-        let end1 = salary - finish
-        console.log(`Sorry. You need ${end1.toFixed(2)}lv. more.`)
-    }
-   }
 }
-solve(3265, 4)
+    if(sum.includes(3)){
+        let three = (salary*3)*0.25
+        var allthree = three - ((salary*2) *0.04)
+        
+    }
+    if(sum.includes(4)){
+        var fourth = (allthree * 0.25) + (salary*0.25)
+    }else{
+        fourth = allthree -allthree
+    }
+    var finish = allthree + fourth
+
+    if(finish > salary){
+    let end = finish - salary
+    console.log(`Bravo! You can go to Disneyland and you will have ${end.toFixed(2)}lv. for souvenirs.`)
+    }else if(finish < salary){
+    let end1 = salary - finish
+    console.log(`Sorry. You need ${end1.toFixed(2)}lv. more.`)
+}
+}
+
+
+solve(1000, 4)
